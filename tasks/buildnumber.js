f5530b17a6f7f35ee8dd771a66de6c85a3e3d517
Benjamin Gwynn <xenxier@xenxier.com>
2016-11-19 05:19:08 +0000

    Update buildnumber.js to adhere to the rules of JSLint

--- tasks/buildnumber.js
+++ tasks/buildnumber.js
@@ -1,58 +1,59 @@
-/*
- * grunt-build-number
- * https://github.com/creynders/grunt-build-number
- *
- * Copyright (c) 2014 Camille Reynders
- * Licensed under the MIT license.
- */
+(function () {
+	/*
+	*	grunt-build-number
+	*	https://github.com/creynders/grunt-build-number
+	*
+	*	Copyright (c) 2014 Camille Reynders
+	*	Copyright (c) 2016 Benjamin Gwynn (http://xenxier.com)
+	*	Licensed under the MIT license.
+	*/
 
-'use strict';
+	'use strict';
+	/*jslint node:true*/
 
-var util = require( 'util' );
+	var util = require('util'),
+		findup = require('findup-sync');
 
-var findup = require( 'findup-sync' );
+	module.exports = function (grunt) {
+		var files = this.files,
+			filepath,
+			meta,
+			buildNum,
+			options = this.options({ // Default options if none provided
+				field : 'build'
+			});
 
-module.exports = function( grunt ){
+		grunt.registerMultiTask('buildnumber', 'Grunt plugin for maintaining a build number in package.json', function () {
+			if (!files.length) {
+				files = [
+					{
+						src : ["package.json"]
+					}
+				];
+			}
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+			files.forEach(function (file) {
+				file.src.forEach(function (filename) {
+					filepath = findup(filename);
 
-  grunt.registerMultiTask( 'buildnumber', 'Grunt plugin for maintaining a build number in package.json', function(){
+					if (filepath && grunt.file.exists(filepath)) {
+						meta = grunt.file.readJSON(filepath);
+						buildNum = meta[options.field];
 
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options( {
-      field : 'build'
-    } );
+						if (typeof buildNum !== "undefined") {
+							buildNum = parseInt(buildNum, 10) + 1;
+						} else {
+							buildNum = 1;
+						}
 
-    var files = this.files;
-    if( !files.length ){
-      files = [
-        {
-          src : ["package.json"]
-        }
-      ];
-    }
-
-    files.forEach( function( file ){
-      file.src.forEach( function( filename ){
-        var filepath = findup( filename );
-        if( filepath && grunt.file.exists( filepath ) ){
-          var meta = grunt.file.readJSON( filepath );
-          var buildNum = meta[options.field];
-          if( typeof buildNum !== "undefined" ){
-            buildNum = parseInt( buildNum, 10 ) + 1;
-          }else{
-            //create build field
-            buildNum = 1;
-          }
-          meta[options.field] = buildNum.toString();
-          grunt.file.write( filepath, JSON.stringify( meta, null, 2 ) );
-          grunt.log.oklns( 'Build number set to "' + buildNum + '" in "' + filename + '".' );
-        }else{
-          grunt.fail.fatal( "file(s) not found: " + file.orig.src );
-        }
-      } );
-    } );
-  } );
-
-};
+						meta[options.field] = buildNum.toString();
+						grunt.file.write(filepath, JSON.stringify(meta, null, 2));
+						grunt.log.oklns('Build number set to "' + buildNum + '" in "' + filename + '".');
+					} else {
+						grunt.fail.fatal("file(s) not found: " + file.orig.src);
+					}
+				});
+			});
+		});
+	};
+}());
