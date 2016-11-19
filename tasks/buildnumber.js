71602c92072e636fe7185982c90fd340fd0f55d2
Benjamin Gwynn <xenxier@xenxier.com>
2016-11-19 07:40:21 +0000

    Add support for keeping indentation in files after parsing them with JSON stringify

--- tasks/buildnumber.js
+++ tasks/buildnumber.js
@@ -11,16 +11,30 @@
 	'use strict';
 	/*jslint node:true*/
 
-	var findup = require('findup-sync');
+	var findup = require('findup-sync'),
+		S = require('string');
 
 	module.exports = function (grunt) {
+
 		grunt.registerMultiTask('buildnumber', 'Grunt plugin for maintaining a build number in package.json', function () {
 			var files = this.files,
+				useTabsInsteadOfSpaces = false,
 				filepath,
-				meta,
+				contents,
+				json,
 				buildNum,
+				newLineMatches,
+				newLineMatchIndex,
+				cleanNewLineMatch,
+				spaceCount = 0,
+				stringified,
+				lines,
+				spaceIndex,
+				space = '',
 				options = this.options({ // Default options if none provided
-					field : 'build'
+					field: 'build',
+					dontChangeIndentation: true,
+					forceTabIndentation: false
 				});
 
 			if (!files.length) {
@@ -36,8 +50,41 @@
 					filepath = findup(filename);
 
 					if (filepath && grunt.file.exists(filepath)) {
-						meta = grunt.file.readJSON(filepath);
-						buildNum = meta[options.field];
+						contents = new S(grunt.file.read(filepath));
+
+						if (options.dontChangeIndentation) {
+							// use this regular expression to get all the new lines and their leading spaces
+							newLineMatches = contents.match(new RegExp("\n *", "g"));
+
+							for (newLineMatchIndex = 0; newLineMatchIndex < newLineMatches.length; newLineMatchIndex += 1) {
+								newLineMatches = new S(newLineMatches[newLineMatchIndex]);
+
+								// remove new line characters
+								cleanNewLineMatch = newLineMatches.replaceAll('\n', '');
+
+								// count space characters, the only characters remaining
+								spaceCount = cleanNewLineMatch.length;
+
+								if (spaceCount) {
+									break;
+								}
+							}
+
+							// if there is still no spacecount
+							if (!spaceCount) {
+								// default to 2 spaces
+								spaceCount = 2;
+
+								// if contains a space use spaces instead
+								useTabsInsteadOfSpaces = contents.contains('\t');
+							}
+						} else {
+							// default to 2 spaces
+							spaceCount = 2;
+						}
+
+						json = JSON.parse(contents);
+						buildNum = json[options.field];
 
 						if (buildNum !== undefined) {
 							buildNum = parseInt(buildNum, 10) + 1;
@@ -45,8 +92,24 @@
 							buildNum = 1;
 						}
 
-						meta[options.field] = buildNum.toString();
-						grunt.file.write(filepath, JSON.stringify(meta, null, 2));
+						json[options.field] = buildNum.toString();
+						stringified = JSON.stringify(json, null, spaceCount);
+
+						if (useTabsInsteadOfSpaces || options.forceTabIndentation) {
+							for (spaceIndex = 0; spaceIndex < spaceCount; spaceIndex += 1) {
+								space += ' ';
+							}
+
+							lines = stringified.split('\n');
+							stringified = '';
+							lines.forEach(function (line) {
+								line = new S(line);
+								line = line.replaceAll(space, '\t');
+								stringified += line + '\n';
+							});
+						}
+
+						grunt.file.write(filepath, stringified);
 						grunt.log.oklns('Build number set to "' + buildNum + '" in "' + filename + '".');
 					} else {
 						grunt.fail.fatal("file(s) not found: " + file.orig.src);
